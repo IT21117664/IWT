d0a1d4a72388146d49dcae44b6360088a3709b34
IT21117664 <it21117664@my.sliit.lk>
2021-09-24 22:13:47 +0530

    config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$con = new mysqli("localhost", "root", "", "lms");
+$con = new mysqli("localhost", "root", "", "lims");
 
 if ($con -> connect_error){
     die("Error" . $con -> connect_error);
