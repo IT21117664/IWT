b6ed93b36b3ccb4ddc429d72008de95baa0c16d8
IT21117664 <it21117664@my.sliit.lk>
2021-09-25 23:30:56 +0530

    js link

--- Add_new_book.php
+++ Add_new_book.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="css/main.css">
+    <script src="./js/main.js" type="text/javascript"></script>
 </head>
 <body>
     <center><h2>ADD NEW BOOKS</h2></center>
