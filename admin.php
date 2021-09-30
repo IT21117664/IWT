2e2ac4539f1ec31cea494225728dd4d5d3749ff4
IT21117664 <it21117664@my.sliit.lk>
2021-09-30 20:19:25 +0530

    RMB

--- admin.php
+++ admin.php
@@ -41,8 +41,8 @@
     </div>
     <div class="row">
         <div class="center">
-            <button type="button" class="btn warning">Lend Book</button>
-            <button type="button" class="btn danger">Retrive Book</button>
+            <button type="button" class="btn danger">Lend Book</button>
+            <button type="button" class="btn warning">Retrive Book</button>
         </div>
     </div>
 
