9c5f84b0a4a35ed1d71721edeea61b0d891ce634
IT21117664 <it21117664@my.sliit.lk>
2021-10-01 12:45:05 +0530

    RMB

--- admin.php
+++ admin.php
@@ -29,10 +29,13 @@
 
     <div class="nav">
         <ul>
-            <li><a  href="#home">Home</a></li>
-            <li><a class="active" href="#news">News</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li><a href="#about">About</a></li>
+            <li><a href="#Library">Library</a></li>
+            <li><a href="#news">Home</a></li>
+            <li><a href="#contact">Articles</a></li>
+            <li><a href="#about">Past Papers</a></li>
+            <li><a href="#about">Reports</a></li>
+            <li><a href="#about">Journals</a></li>
+            <li><a href="#about" class="active">Admin</a></li>
         </ul>
     </div>
 
@@ -63,24 +66,23 @@
 
     <div class="row">
         <div class="column">
-            <h3 align="left">Member</h3>
-            <input type="button" value="New Member Validation" class="btn border colorprimary">
-            <input type="button" value="Add Memeber" class="btn border colorprimary">
-            <input type="button" value="Member Details" class="btn border colorprimary">
+            <h3 align="left">Books</h3>
+            <input type="button" value="Add New Books" class="btn border colorprimary">
+            <input type="button" value="Book Detail" class="btn border colorprimary">
+            <input type="button" value="Request New Books" class="btn border colorprimary">
         </div>
     </div>
 
     <div class="row">
         <div class="column">
-            <h3 align="left">Member</h3>
-            <input type="button" value="New Member Validation" class="btn border colorprimary">
-            <input type="button" value="Add Memeber" class="btn border colorprimary">
-            <input type="button" value="Member Details" class="btn border colorprimary">
+            <h3 align="left">IT Technician</h3>
+            <input type="button" value="Error Submission" class="btn border colorprimary">
+            <input type="button" value="Contact Detail" class="btn border colorprimary">
         </div>
     </div>
 
     <div class="footer">
-        <p>Home | </p>
+        <p>E - book | Reports | Journals | Library Police | Contact Us<br>Copyright SLIIT &copy; 2021 - All right reserved</p>
     </div>
     <script> carousel();</script>
 </body>
