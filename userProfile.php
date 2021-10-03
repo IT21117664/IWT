b5fa84089a88af6a218f3d40bbe6f71ae4924c5a
IT21117664 <it21117664@my.sliit.lk>
2021-10-03 10:46:38 +0530

    profile

---
+++ userProfile.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SLIIT ONLINE LIBRARY - ADMIN</title>
+    <link rel="stylesheet" href="./css/main.css">
+    <script src="https://kit.fontawesome.com/07c9a11431.js" crossorigin="anonymous"></script>
+    <script src="./js/main.js"></script>
+</head>
+<body>
+    <div class="slide">
+        <div class="slidecaption">
+            <img class="mainSlide" src="./img/Slide/1.jpg" style="width:100%">
+            <div class="topRight">
+                <h3>RMB
+                <input type="button" value="Button" class="btn primary"></h3>
+            </div>
+
+            <div class="topLeft">
+                <h3>Time</h3>
+            </div>
+
+        </div>
+        <img class="mainSlide" src="./img/Slide/2.jpg" style="width:100%">
+        <img class="mainSlide" src="./img/Slide/3.jpg" style="width:100%">
+    </div>
+
+    <div class="nav">
+        <ul>
+            <li><a href="#Library">Library</a></li>
+            <li><a href="#news">Home</a></li>
+            <li><a href="#contact">Articles</a></li>
+            <li><a href="#about">Past Papers</a></li>
+            <li><a href="#about">Reports</a></li>
+            <li><a href="#about">Journals</a></li>
+        </ul>
+    </div>
+
+    <div class="row">
+        <div class="column side"></div>
+        <div class="column middle">
+            <div class="profile">
+            <div class="card">
+                <img src="img_avatar.png" alt="Avatar" style="width:100%">
+                <div class="container">
+                    <h4><b>John Doe</b></h4> 
+                    <p>Architect & Engineer</p> 
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer">
+        <p>E - book | Reports | Journals | Library Police | Contact Us<br>Copyright SLIIT &copy; 2021 - All right reserved</p>
+    </div>
+    <script> carousel();</script>
+</body>
+</html>
