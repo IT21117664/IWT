744a951ae651273a7710a605b9f0792d311948a6
kirula0626 <kirula02@gmail.com>
2021-10-04 22:04:23 +0530

    ex

--- config.php
+++ config.php
@@ -3,6 +3,10 @@ $con = new mysqli("localhost", "root", "", "lims");
 
 if ($con -> connect_error){
     die("Error" . $con -> connect_error);
-    //Connectionr
+    
 }
+else {
+    echo("Connection Success");
+}
+
 ?>
