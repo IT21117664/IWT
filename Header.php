4a22781b1cef768e952d3b1aeaf80de8648c76d1
IT21117664 <it21117664@my.sliit.lk>
2021-10-07 19:11:20 +0530

    RMB

--- Header.php
+++ Header.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script src="./js/main.js"></script>
-</head>
-<body>
 <header>
     <div class="slide">
         <div class="slidecaption">
@@ -41,8 +31,7 @@
 
     <script>
         carousel();
+        setInterval(time, 1000);
     </script>
 
-</header> 
-</body>
-</html>
+</header>
