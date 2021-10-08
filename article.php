863b25b4f1d2ffe1f5c26ae366db4f89ac610b89
IT21106156 <it21106156@my.sliit.lk>
2021-10-08 18:28:34 +0530

    ggg

--- article.php
+++ article.php
@@ -18,11 +18,11 @@
     <?php
 
     require('header.php');
-    logIn();
+
 
     ?>
 
-<div class="nav">
+    <div class="nav">
         <ul>
             <li><a class="logoL">Library</a></li>
             <li><a href="#news">Home</a></li>
@@ -34,68 +34,234 @@
     </div><br>
 
 
-    
-    <form id="form">
-        <input type="search" id="query" name="q" img src="C:\Users\MSii\Desktop\search.jpg">
-        <button>Search</button>
-    </form>
 
-    <a href="#" style="text-align: left;">Advanced Search</a>
-
- 
     <div class="row">
-        <div class="column micro"></div>
-        <div class="column large">
-            <div class="card_column">
-                <div id="book1" class="card">
-                    <img src="./img/logo.png" alt="book img">
-                    <div><label>Book Name</label></div>
-                    <div><label>Authot</label></div>
-                    <div><label>ISBN</label></div>
-                </div>
-            </div>
+        <div class="column side"></div>
+        <div class="column middle">
+            <input type="text" class="txtSearch" placeholder="Search...">
+            <input type="button" value="Search" class="btn primary">
+            <a href="#" class="topRighta">Advanced Search</a>
+            <div style="width: 100%;">
 
-
-            <div class="card_column">
-                <div id="book2" class="card">
-                    <img src="./img/logo.png" alt="book img">
-                    <div><label>Book Name</label></div>
-                    <div><label>Authot</label></div>
-                    <div><label>ISBN</label></div>
-                </div>
-            </div>
-
-            <div class="card_column">
-                <div id="book3" class="card">
-                    <img src="./img/logo.png" alt="book img">
-                    <div><label>Book Name</label></div>
-                    <div><label>Authot</label></div>
-                    <div><label>ISBN</label></div>
-                </div>
-            </div>
-
-            <div class="card_column">
-                <div id="book4" class="card">
-                    <img src="./img/logo.png" alt="book img">
-                    <div><label>Book Name</label></div>
-                    <div><label>Authot</label></div>
-                    <div><label>ISBN</label></div>
-                </div>
-            </div>
-
-            <div class="card_column">
-                <div id="book5" class="card">
-                    <img src="./img/logo.png" alt="book img">
-                    <div><label>Book Name</label></div>
-                    <div><label>Authot</label></div>
-                    <div><label>ISBN</label></div>
-                </div>
             </div>
         </div>
+
     </div>
 
 
 
+    <div class="row">
+        <div class="">
+            <div class="card">
+                <h1>New Arrivals</h1>
+            </div>
+        </div>
+
+        <?php
+        require('config.php');
+
+
+        // if (isset($_REQUEST['IID'])) {
+        $IID = $_REQUEST['IID'];
+
+        $sqlSelectBook = "SELECT i.Name, i.free, i.itemImgLoc, i.pdfPath, i.Description FROM inventory AS i WHERE  i.IID = '" . $IID . "' ORDER BY `Published_Date` DESC LIMIT 5 OFFSET 0;";
+        $resultSelectBook = $con->query($sqlSelectBook);
+        if ($resultSelectBook->num_rows > 0) {
+            $numberofrows = $resultSelectBook->num_rows;
+            for ($i = 0; $i < $numberofrows; $i++) {
+
+                while ($rowSelectBook = $resultSelectBook->fetch_assoc()) {
+
+                    $Name = $rowSelectBook['Name'];
+                    $itemImgLoc = $rowSelectBook['itemImgLoc'];
+                    $free = $rowSelectBook['free'];
+
+                    $sqlSelectaouter = "SELECT * FROM author WHERE  AID  = '" . $rowSelectBook["A_ID"] . "';";
+                    $resulsetauther = $con->query($sqlSelectcatery);
+
+                    while ($rowSelectarther = $resulsetauther->fetch_assoc()) {
+                        $aouthername =  $rowSelectarther["catName"];
+                    }
+
+
+        ?>
+                    <div class="column micro"></div>
+                    <div class="column large">
+                        <div class="card_column">
+                            <div id="book1" class="card">
+                                <img src="$itemImgLoc" alt="book img">
+                                <div><label><?php echo $Name; ?></label></div>
+                            </div>
+                        </div>
+
+
+            <?php
+                }
+            }
+        }
+        // } else {
+        //     // header("Location: ./index.php?error=bookView");
+        // }
+            ?>
+
+
+            <div class="column micro"></div>
+            <div class="column large">
+                <div class="card_column">
+                    <div id="book1" class="card">
+                        <img src="./img/book/13.png" alt="book img">
+                        <div><label>Book Name</label></div>
+                    </div>
+                </div>
+
+
+                <div class="card_column">
+                    <div id="book2" class="card">
+                        <img src="./img/logo.png" alt="book img">
+                        <div><label>Book Name</label></div>
+
+                    </div>
+                </div>
+
+                <div class="card_column">
+                    <div id="book3" class="card">
+                        <img src="./img/logo.png" alt="book img">
+                        <div><label>Book Name</label></div>
+
+                    </div>
+                </div>
+
+                <div class="card_column">
+                    <div id="book4" class="card">
+                        <img src="./img/logo.png" alt="book img">
+                        <div><label>Book Name</label></div>
+                        
+                    </div>
+                </div>
+
+                <div class="card_column">
+                    <div id="book5" class="card">
+                        <img src="./img/logo.png" alt="book img">
+                        <div><label>Book Name</label></div>
+                       
+                    </div>
+                </div>
+            </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="">
+                            <div class="card">
+                                <h1>Trending</h1>
+                            </div>
+                        </div>
+
+
+
+                        <div class="column micro"></div>
+                        <div class="column large">
+                            <div class="card_column">
+                                <div id="book1" class="card">
+                                    <img src="./img/logo.png" alt="book img">
+                                    <div><label>Book Name</label></div>
+                                    
+                                </div>
+                            </div>
+
+
+                            <div class="card_column">
+                                <div id="book2" class="card">
+                                    <img src="./img/logo.png" alt="book img">
+                                    <div><label>Book Name</label></div>
+                                    
+                                </div>
+                            </div>
+
+                            <div class="card_column">
+                                <div id="book3" class="card">
+                                    <img src="./img/logo.png" alt="book img">
+                                    <div><label>Book Name</label></div>
+                                    
+                                </div>
+                            </div>
+
+                            <div class="card_column">
+                                <div id="book4" class="card">
+                                    <img src="./img/logo.png" alt="book img">
+                                    <div><label>Book Name</label></div>
+                                   
+                                </div>
+                            </div>
+
+                            <div class="card_column">
+                                <div id="book5" class="card">
+                                    <img src="./img/logo.png" alt="book img">
+                                    <div><label>Book Name</label></div>
+                                   
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="">
+                            <div class="card">
+                                <h1>Suggestions</h1>
+                            </div>
+                        </div>
+
+
+
+                        <div class="column micro"></div>
+                        <div class="column large">
+                            <div class="card_column">
+                                <div id="book1" class="card">
+                                    <img src="./img/logo.png" alt="book img">
+                                    <div><label>Book Name</label></div>
+                                   
+                                </div>
+                            </div>
+
+
+                            <div class="card_column">
+                                <div id="book2" class="card">
+                                    <img src="./img/logo.png" alt="book img">
+                                    <div><label>Book Name</label></div>
+                                   
+                                </div>
+                            </div>
+
+                            <div class="card_column">
+                                <div id="book3" class="card">
+                                    <img src="./img/logo.png" alt="book img">
+                                    <div><label>Book Name</label></div>
+                                    
+                                </div>
+                            </div>
+
+                            <div class="card_column">
+                                <div id="book4" class="card">
+                                    <img src="./img/logo.png" alt="book img">
+                                    <div><label>Book Name</label></div>
+                                    
+                                </div>
+                            </div>
+
+                            <div class="card_column">
+                                <div id="book5" class="card">
+                                    <img src="./img/logo.png" alt="book img">
+                                    <div><label>Book Name</label></div>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <?php
+
+                    require "Footer.php";
+                    ?>
 
 
 
