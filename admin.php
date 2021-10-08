288aecfe29910b0d6722f82eb69579e972ca9f35
IT21117664 <it21117664@my.sliit.lk>
2021-10-08 13:06:08 +0530

    RMB

--- admin.php
+++ admin.php
@@ -1,31 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SLIIT ONLINE LIBRARY - ADMIN</title>
-    <link rel="stylesheet" href="./css/main.css">
-    <script src="https://kit.fontawesome.com/07c9a11431.js" crossorigin="anonymous"></script>
-    <script src="./js/main.js"></script>
-</head>
-<body>
-    <div class="slide">
-        <div class="slidecaption">
-            <img class="mainSlide" src="./img/Slide/1.jpg" style="width:100%">
-            <div class="topRight">
-                <h3>RMB
-                <input type="button" value="Button" class="btn primary"></h3>
-            </div>
-
-            <div class="topLeft">
-                <h3>Time</h3>
-            </div>
-
-        </div>
-        <img class="mainSlide" src="./img/Slide/2.jpg" style="width:100%">
-        <img class="mainSlide" src="./img/Slide/3.jpg" style="width:100%">
-    </div>
+<?php require('Header.php'); ?>
 
     <div class="nav">
         <ul>
