580bd57df40525a575c2253a7bc524a649a37567
it21104558 <it21104558@my.sliit.lk>
2021-10-09 23:00:11 +0530

    ghghgfh

--- forget_password.php
+++ forget_password.php
@@ -19,10 +19,11 @@
 
                 <tr>
                     <td>
-                    Username : <input type="text" class="pop-table Memdel" id="username" name="username">
+                   <label class="" Username : <input type="text" class="pop-table Memdel" id="username" name="username">
                     <button type="submit" class="check">Check</button>
                     </td>
                 </tr>
+ 
                 <tr>
                     <td>
                     Sliit Email: <input type="text" class="pop-table Memde" id="Sliit Email" name="Sliit Email">
@@ -32,14 +33,9 @@
             </form>
             </table>    
 
-        </div>
+        </div> <br><br><br><br><br>
 
-        <footer>
-            <div class="footer">
-                <p><a href="#" class="footLink">E - book</a> | <a href="#" class="footLink">Reports</a> | <a href="#" class="footLink">Journals</a> | <a href="#" class="footLink">Library</a>Police</a> | <a href="#" class="footLink">Contact Us</a><br>Copyright SLIIT
-                    &copy; 2021 - All right reserved</p>
-            </div>
-        </footer>
+        <?php require('footer.php'); ?>
     
 
     
