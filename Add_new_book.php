25c2fe9a765a4ede92a14c248a4af0ddefa84f0d
it21104558 <it21104558@my.sliit.lk>
2021-10-09 23:01:18 +0530

    dvgbcbvb

--- Add_new_book.php
+++ Add_new_book.php
@@ -111,12 +111,7 @@
 
         
 
-        <footer>
-            <div class="footer">
-                <p><a href="#" class="footLink">E - book</a> | <a href="#" class="footLink">Reports</a> | <a href="#" class="footLink">Journals</a> | <a href="#" class="footLink">Library</a>Police</a> | <a href="#" class="footLink">Contact Us</a><br>Copyright SLIIT
-                    &copy; 2021 - All right reserved</p>
-            </div>
-        </footer>
+            <?php require('footer.php'); ?>
 
 
 </body>
--- book_details.php
+++ book_details.php
@@ -106,14 +106,9 @@
                         <input type="submit" id="Book_Details_Button" name="Book_Details_Button" class="btn-pop mem btn-update" value="update">
                     </div>
 
-            </div>
+            </div> 
 
-        <footer>
-            <div class="footer">
-                <p><a href="#" class="footLink">E - book</a> | <a href="#" class="footLink">Reports</a> | <a href="#" class="footLink">Journals</a> | <a href="#" class="footLink">Library</a>Police</a> | <a href="#" class="footLink">Contact Us</a><br>Copyright SLIIT
-                    &copy; 2021 - All right reserved</p>
-            </div>
-        </footer>
+            <?php require('footer.php'); ?>
 
 
 
