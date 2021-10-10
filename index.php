f17d0b3209ebb78bf7835a0ec4caab9ddcb39fa0
IT21114526 <it21114526@my.sliit.lk>
2021-10-10 18:21:39 +0530

    hari

--- index.php
+++ index.php
@@ -14,25 +14,14 @@
                 $bookImg = $bookArray['itemImgLoc'];
                 $bookID = $bookArray['IID'];
 
-                $output .=  "
-                <div class=\"card set\">
-                    <div class=\"card title\">
-                        <h1>New Arrivals</h1>
-                    </div>
-                    <div class=\"row\">
-                    <div class=\"column micro\"></div>
-                <div class=\"column large\">
-                    <div class=\"card_column\">
-                        <div id=\"book1\" class=\"card\">
+                $output .=  "<div class=\"card_column\"><div class=\"card\">
                             <img src=\"$bookImg\" alt=\"$bookName\">
                             <div><label>$bookName</label></div>
                             <div><label></label></div>
                             <div><label>$bookName</label></div>
                             <div><label>$bookAuthor</label></div>
-                        </div>
-                    </div>
-                    </div>
-                    </div>";
+                            </div>
+                        </div>";
             }
         }
     
@@ -40,61 +29,21 @@
         header("Location: ./index.php?error=search");
         }
 ?>
-
-<div class="card set">
-        <div class="card title">
-            <h1>Trending</h1>
-        </div>
-
-
-    <div class="list_box">
-        <div class="row">
-            <div class="column_list">
-                <div class="card">
-
-                    <div><a href="book details.php">New Arrivals</a></div>
-                    <ul>
-                        <li><a href="book details.php">book 1</a></li>
-                        <li> <a href="book details.php">book 2</a></li>
-                        <li><a href="book details.php">book 3</a></li>
-                        <li> <a href="book details.php">book 4</a></li>
-                        <li><a href="book details.php">book 5</a></li>
-                        <li> <a href="book details.php">book 6</a></li>
-                    </ul>
+    <div class="column micro"></div>
+        <div class="column large">
+            <div class="card set">
+                <div class="card title">
+                    <h1>New Arrivals</h1>
                 </div>
-            </div>
-
-
-            <div class="column_list">
-                <div class="card">
-                    <div><a href="book details.php">Trending</a></div>
-                    <ul>
-                        <li><a href="book details.php">book 1</a></li>
-                        <li> <a href="book details.php">book 2</a></li>
-                        <li><a href="book details.php">book 3</a></li>
-                        <li> <a href="book details.php">book 4</a></li>
-                        <li><a href="book details.php">book 5</a></li>
-                        <li> <a href="book details.php">book 6</a></li>
-                    </ul>
-                </div>
-            </div>
-
-
-            <div class="column_list">
-                <div class="card">
-                    <div><a href="book details.php">Most rated</a></div>
-                    <ul>
-                        <li><a href="book details.php">book 1</a></li>
-                        <li> <a href="book details.php">book 2</a></li>
-                        <li><a href="book details.php">book 3</a></li>
-                        <li> <a href="book details.php">book 4</a></li>
-                        <li><a href="book details.php">book 5</a></li>
-                        <li> <a href="book details.php">book 6</a></li>
-                    </ul>
+                <div class="row">
+                        <?php echo $output; ?>
                 </div>
             </div>
         </div>
     </div>
+
+
+    
 
 
 </body>
