3fd30e537df1b9c503d614a43c53b06557db8386
IT21117664 <it21117664@my.sliit.lk>
2021-10-10 20:17:59 +0530

    js

--- js/main.js
+++ js/main.js
@@ -15,35 +15,6 @@ function carousel() {
 
 //------------------------------ /Slider ---------------------------------------------
 
-//------------------------------ Books preview----------------------------------------
-
-/*
-var bookIMG = 0;
-
-function bookpreview() {
-    var i;
-    var x = document.getElementsByClassName("bookView");
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    bookIMG++;
-    if (bookIMG > x.length) { bookIMG = 1 }
-    x[bookIMG - 1].style.display = "block";
-    setTimeout(carousel, 2000); // Change image every 2 seconds
-}
-<<<<<<< HEAD
-
-//------------------------------ /Books preview----------------------------------------
-
-
-=======
-*/
-//------------------------------ /Books preview--------------------------------
-
-
-
-
-
 //------------------------------Time---------------------------------------------------
 function time() {
     var date_time = new Date();
