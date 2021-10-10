ad5787b2422ba8d6a2c55e6a21f510563d183721
IT21117664 <it21117664@my.sliit.lk>
2021-10-10 21:45:23 +0530

    test

--- register.php
+++ register.php
@@ -1,44 +1,40 @@
 <?php
- if(isset($_POST['fname'])){
-        $firstName = $_POST['fname'];
-        $lastName = $_POST['lname'];
-        $nameWithInitials = $_POST['nwi'];
-        $sliitID = $_POST['regNO'];
-        $email = $_POST['email'];
-        $dateOfBorth = $_POST['dob'];
-        $mobile = $_POST['mobile'];
-        $address = $_POST['address'];
-        $imgLink = $_POST['imgLink'];
-        $sliitMail = $_POST['sliitMail'];
-        $password = $_POST['pwd'];
-        $conform_password = $_POST['conformPwd'];
-        $NIC = $_POST['NIC'];
+if (isset($_POST['fname'])) {
+    $firstName = $_POST['fname'];
+    $lastName = $_POST['lname'];
+    $nameWithInitials = $_POST['nwi'];
+    $sliitID = $_POST['regNO'];
+    $email = $_POST['email'];
+    $dateOfBorth = $_POST['dob'];
+    $mobile = $_POST['mobile'];
+    $address = $_POST['address'];
+    $imgLink = $_POST['imgLink'];
+    $sliitMail = $_POST['sliitMail'];
+    $password = $_POST['pwd'];
+    $conform_password = $_POST['conformPwd'];
+    $NIC = $_POST['NIC'];
 
-        if($password == $conform_password){
-             $regSql = "INSERT INTO `user`( `FName`, `LName`, `NameWithInitial`, `userID`, `email`, `DateOfBirth`, `phoneNumber`, `Address`, `profileImg`, `Password`, `NIC`) VALUES (\"$fname\", \"$lname\",\"$nwi\", \"$regNo\", \"$email\", \"$dateOfBorth\", \"$mobile\", \"$address\", \"$imgLink\", \"$pwd\", \"$NIC\" )";
-             if($con -> query($sqlLoadHistory)){
-                header("Location: ./index.php?msg=success");
-             }else{
-                header("Location: ./index.php?msg=error");
-             }
+    if ($password == $conform_password) {
+        $regSql = "INSERT INTO `user`( `FName`, `LName`, `NameWithInitial`, `userID`, `email`, `DateOfBirth`, `phoneNumber`, `Address`, `profileImg`, `Password`, `NIC`) VALUES (\"$fname\", \"$lname\",\"$nwi\", \"$regNo\", \"$email\", \"$dateOfBorth\", \"$mobile\", \"$address\", \"$imgLink\", \"$pwd\", \"$NIC\" )";
+        if ($con->query($sqlLoadHistory)) {
+            header("Location: ./index.php?msg=success");
+        } else {
+            header("Location: ./index.php?msg=error");
         }
-
-        else{
-            echo"
+    } else {
+        echo "
             <script>
             alert('Password does not match');
             </script>";
-
-        }
-
     }
-    
-    $adminShow = "";
-    if (isset($_SESSION['userID'])){
-        if ($_SESSION['userType'] == 1){
-            $adminShow = "<li><a href=\"admin.php\">Admin</a></li>";
-        }
+}
+
+$adminShow = "";
+if (isset($_SESSION['userID'])) {
+    if ($_SESSION['userType'] == 1) {
+        $adminShow = "<li><a href=\"admin.php\">Admin</a></li>";
     }
+}
 ?>
 
 
@@ -56,11 +52,11 @@
 
 <body>
     <?php
-        include "Header.php";
-        
+    include "Header.php";
+
     ?>
 
-   <div class="nav">
+    <div class="nav">
         <ul>
             <li><a href="#Library">Library</a></li>
             <li><a href="index.php">Home</a></li>
@@ -71,145 +67,145 @@
             <?php echo $adminShow; ?>
         </ul>
 
-            <div class="wrapper">
-                <div class="card">
-                    <div class="cardDetails">
-                        <div class="row">
-                            <form method="post" class="form">
-                                <div class="personal">
-                                    <div>
-                                        <h2>Personal Information</h2>
-                                    </div>
-                                    <table>
-                                        <tr>
-                                            <td>
-                                                <div class="input_item">
-                                                    <label for="fname">First Name</label>
-                                                    <input type="text" id="fname" name="fname" required class="txt input_box" />
-                                                </div>
-                                            </td>
-
-                                            <td>
-                                                <div class="input_item">
-                                                    <label for="lname">Last Name</label>
-                                                    <input type="text" id="lname" name="lname" required class="txt input_box" />
-                                                </div>
-                                            </td>
-                                        </tr>
-
-                                        <tr>
-                                            <td colspan="2">
-                                                <div class="input_item">
-                                                    <label for="nwi">Name With Initials</label>
-                                                    <input type="text" id="nwi" name="nwi" required class="txt input_box" />
-                                                </div>
-                                            </td>
-                                        </tr>
-
-                                        <tr colspan = '2'>
-                                            <td>
+        <div class="wrapper">
+            <div class="card">
+                <div class="cardDetails">
+                    <div class="row">
+                        <form method="post" class="form">
+                            <div class="personal">
+                                <div>
+                                    <h2>Personal Information</h2>
+                                </div>
+                                <table>
+                                    <tr>
+                                        <td>
                                             <div class="input_item">
-                                                    <label for="mail">E-mail</label>
-                                                    <input type="email" id="email" name="email" onkeyup="mailtyping()" required class="txt input_box" />
-                                                </div>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                <div class="input_item">
-                                                    <label for="regNO">SLIIT Reg No.</label>
-                                                    <input type="text" id="regNO" name="regNO" required class="txt input_box" />
-                                                </div>
-                                            </td>
+                                                <label for="fname">First Name</label>
+                                                <input type="text" id="fname" name="fname" required class="txt input_box" />
+                                            </div>
+                                        </td>
 
-                                            <td>
-                                                <div class="input_item">
-                                                    <label for="NIC">NIC No.</label>
-                                                    <input type="text" id="NIC" name="NIC" required class="txt input_box" />
-                                                </div>
-                                            </td>
-                                           
-                                        </tr>
+                                        <td>
+                                            <div class="input_item">
+                                                <label for="lname">Last Name</label>
+                                                <input type="text" id="lname" name="lname" required class="txt input_box" />
+                                            </div>
+                                        </td>
+                                    </tr>
 
-                                        <tr>
-                                            <td>
-                                                <div class="input_item">
-                                                    <label for="dob">Date Of Birth</label>
-                                                    <input type="date" id="dob" name="dob" required class="txt input_box" value="Today">
-                                                </div>
-                                            </td>
-                                            <td>
-                                                <div class="input_item">
-                                                    <label for="mobile">Mobile Number</label>
-                                                    <input type="text" id="mobile" name="mobile" required class="txt input_box" />
-                                                </div>
-                                            </td>
-                                        </tr>
-                                        <tr row>
-                                            <td colspan="2">
-                                                <div class="input_item">
-                                                    <label for="address">Address</label>
-                                                    <input type="text" id="address" name="address" required class="txt input_box" />
-                                                </div>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                <div class="input_item">
-                                                    <label for="imgLink">Upload Image</label>
-                                                    <input type="file" id="imgLink" name="imgLink" />
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    </table>
+                                    <tr>
+                                        <td colspan="2">
+                                            <div class="input_item">
+                                                <label for="nwi">Name With Initials</label>
+                                                <input type="text" id="nwi" name="nwi" required class="txt input_box" />
+                                            </div>
+                                        </td>
+                                    </tr>
+
+                                    <tr colspan='2'>
+                                        <td>
+                                            <div class="input_item">
+                                                <label for="mail">E-mail</label>
+                                                <input type="email" id="email" name="email" onkeyup="mailtyping()" required class="txt input_box" />
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <div class="input_item">
+                                                <label for="regNO">SLIIT Reg No.</label>
+                                                <input type="text" id="regNO" name="regNO" required class="txt input_box" />
+                                            </div>
+                                        </td>
+
+                                        <td>
+                                            <div class="input_item">
+                                                <label for="NIC">NIC No.</label>
+                                                <input type="text" id="NIC" name="NIC" required class="txt input_box" />
+                                            </div>
+                                        </td>
+
+                                    </tr>
+
+                                    <tr>
+                                        <td>
+                                            <div class="input_item">
+                                                <label for="dob">Date Of Birth</label>
+                                                <input type="date" id="dob" name="dob" required class="txt input_box" value="Today">
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div class="input_item">
+                                                <label for="mobile">Mobile Number</label>
+                                                <input type="text" id="mobile" name="mobile" required class="txt input_box" />
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <tr row>
+                                        <td colspan="2">
+                                            <div class="input_item">
+                                                <label for="address">Address</label>
+                                                <input type="text" id="address" name="address" required class="txt input_box" />
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <div class="input_item">
+                                                <label for="imgLink">Upload Image</label>
+                                                <input type="file" id="imgLink" name="imgLink" />
+                                            </div>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </div>
+
+                            <div class="login">
+                                <div>
+                                    <h2>Login Details</h2>
                                 </div>
 
-                                <div class="login">
-                                    <div>
-                                        <h2>Login Details</h2>
-                                    </div>
+                                <table>
+                                    <tr>
+                                        <td colspan="2">
+                                            <div class="input_item">
+                                                <label for="sliitMail">SLIIT mail</label>
+                                                <input type="email" id="sliitMail" name="sliitMail" required class="txt input_box" />
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <div class="input_item">
+                                                <label for="pwd">Password</label>
+                                                <input type="password" name="pwd" id="pwd" required class="txt input_box" />
+                                            </div>
+                                        </td>
 
-                                    <table>
-                                        <tr>
-                                            <td colspan="2">
-                                                <div class="input_item">
-                                                    <label for="sliitMail">SLIIT mail</label>
-                                                    <input type="email" id="sliitMail" name="sliitMail" required class="txt input_box" />
-                                                </div>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                <div class="input_item">
-                                                    <label for="pwd">Password</label>
-                                                    <input type="password" name="pwd" id="pwd" required class="txt input_box" />
-                                                </div>
-                                            </td>
+                                        <td>
+                                            <div class="input_item">
+                                                <label for="conformPwd">Conform Password</label>
+                                                <input type="password" name="conformPwd" id="conformPwd" required class="txt input_box" min="8" max="64" />
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <label for="pwdCheck" id="pwdCheck" onkeyup="passwordCheck()"></label>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </div>
 
-                                            <td>
-                                                <div class="input_item">
-                                                    <label for="conformPwd">Conform Password</label>
-                                                    <input type="password" name="conformPwd" id="conformPwd" required class="txt input_box" min="8" max="64" />
-                                                </div>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                <label for="pwdCheck" id="pwdCheck" onkeyup="passwordCheck()"></label>
-                                            </td>
-                                        </tr>
-                                    </table>
-                                </div>
-
-                                <div class="right">
-                                    <button type="reset" class="btn danger">Clear</button>
-                                    <button type="submit" class="btn success">Sign Up</button>
-                                </div>
-                            </form>
-                        </div>
+                            <div class="right">
+                                <button type="reset" class="btn danger">Clear</button>
+                                <button type="submit" class="btn success">Sign Up</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
+        </div>
 
         <script>
             email.addEventListener("keypress", mail_auto);
